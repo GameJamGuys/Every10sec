fe1a7b248c2b86ebd258b7377db66b6241e7d0d1
Aristarkh Abramovsky <ari.bram@ya.ru>
2022-10-02 10:45:30 +0300

    Asset changes

--- Assets/_Code/Objects/PlayerLogic.cs
+++ Assets/_Code/Objects/PlayerLogic.cs
@@ -8,11 +8,15 @@ public class PlayerLogic : StaticInstance<PlayerLogic>
     [HideInInspector]
     public int shieldCount;
 
+    public GameObject lCut;
+    public GameObject rCut;
+
     int bossPower;
 
     void Start()
     {
         bossPower = BossLogic.Instance.bossAttack;
+        shield.SetActive(false);
     }
 
     // Update is called once per frame
@@ -31,19 +35,32 @@ public class PlayerLogic : StaticInstance<PlayerLogic>
 
     }
 
+    IEnumerator Blink(GameObject cut)
+    {
+        cut.SetActive(true);
+        yield return new WaitForSeconds(0.1f);
+        cut.SetActive(false);
+    }
+
     public void RecieveAttack(BossAttackType type)
     {
         switch (type)
         {
             case BossAttackType.Left:
                 PlayerManager.Instance.AddHp(-bossPower);
+                Blink(lCut);
                 break;
             case BossAttackType.Right:
                 PlayerManager.Instance.AddHp(-bossPower);
+                Blink(rCut);
                 break;
             case BossAttackType.Both:
                 PlayerManager.Instance.AddHp(-bossPower * 2);
+                Blink(lCut);
+                Blink(rCut);
                 break;
         }
     }
+
+
 }
