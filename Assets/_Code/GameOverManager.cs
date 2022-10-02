5db6509fbceed2abad954c27508040d4eb6e0e7a
1Kirik3 <dikirkota@stud.etu.ru>
2022-10-02 16:40:46 +0300

    gameOverManager
    add gameOverManagerScript

---
+++ Assets/_Code/GameOverManager.cs
@@ -0,0 +1,37 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverManager : StaticInstance<GameOverManager>
+{
+    public bool gameCondition;
+    public bool isGameEnd;
+    public GameObject gameOverBack;
+    public GameObject loseState;
+    public GameObject winState;
+
+    public void Update()
+    {
+        if (isGameEnd == false)
+        {
+            gameOverBack.SetActive(true);
+            
+            if(gameCondition==true) { ShowWin();}
+            else { ShowLose(); }
+
+        }
+    }
+
+    public void ShowLose()
+    {
+        loseState.SetActive(true);
+        winState.SetActive(false);
+    }
+
+    public void ShowWin()
+    {
+        winState.SetActive(true);
+        loseState.SetActive(false);
+    }
+}
